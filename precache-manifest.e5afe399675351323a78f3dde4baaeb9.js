102d57f999a46c50d714ebb3df49ee15cc975da7
reeyavii <94153703+reeyavii@users.noreply.github.com>
2023-02-19 21:57:46 +0800

    Updates

---
+++ precache-manifest.e5afe399675351323a78f3dde4baaeb9.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ef778dc9e60baf5ca9c",
+    "url": "/mclms_prod/static/css/main.543c1f89.chunk.css"
+  },
+  {
+    "revision": "2ef778dc9e60baf5ca9c",
+    "url": "/mclms_prod/static/js/main.62d37ac9.chunk.js"
+  },
+  {
+    "revision": "3d8257dd2c4fb41b5346",
+    "url": "/mclms_prod/static/js/runtime~main.b9f172ad.js"
+  },
+  {
+    "revision": "8dce77535739ecfa237e",
+    "url": "/mclms_prod/static/js/2.728385ed.chunk.js"
+  },
+  {
+    "revision": "9dd5892b0cea874a44aa",
+    "url": "/mclms_prod/static/js/3.e5a6111a.chunk.js"
+  },
+  {
+    "revision": "679d6cfc7c7742fee7867856960e0707",
+    "url": "/mclms_prod/static/media/MarketName.679d6cfc.jpg"
+  },
+  {
+    "revision": "e467e06e3bf6b2298c0909755322631c",
+    "url": "/mclms_prod/static/media/logo-alimodian.e467e06e.png"
+  },
+  {
+    "revision": "160d97f4ccaf1ec8c336805cae432a64",
+    "url": "/mclms_prod/static/media/bg.160d97f4.jpg"
+  },
+  {
+    "revision": "a57a62fb411f088921860bf92979cd0e",
+    "url": "/mclms_prod/static/media/Verified.a57a62fb.png"
+  },
+  {
+    "revision": "57e4721eb18733e5c9fc4751495679e9",
+    "url": "/mclms_prod/static/media/GcashLogo.57e4721e.png"
+  },
+  {
+    "revision": "b48519351be92099287efcdcf19cfdb1",
+    "url": "/mclms_prod/static/media/User.b4851935.png"
+  },
+  {
+    "revision": "01f53da1716f1f3f8d3460519a8287df",
+    "url": "/mclms_prod/static/media/Alim_Logo.01f53da1.png"
+  },
+  {
+    "revision": "a3db1bbe06c57a0926e93ad01cd40c48",
+    "url": "/mclms_prod/static/media/qr.a3db1bbe.jpg"
+  },
+  {
+    "revision": "b57a83a527853827dbdaa48421af5ce7",
+    "url": "/mclms_prod/static/media/Verified1.b57a83a5.png"
+  },
+  {
+    "revision": "35486fbf0435a6131c942d61fd77d4eb",
+    "url": "/mclms_prod/static/media/Stalls.35486fbf.png"
+  },
+  {
+    "revision": "252132e4e1efee01f972ff9764be4383",
+    "url": "/mclms_prod/static/media/Form.252132e4.png"
+  },
+  {
+    "revision": "b57ebe81b2266b7f1ed79dbf244cf632",
+    "url": "/mclms_prod/static/media/Payment.b57ebe81.png"
+  },
+  {
+    "revision": "68ac7170f801879025616cbc4bc948e5",
+    "url": "/mclms_prod/static/media/About Us.68ac7170.png"
+  },
+  {
+    "revision": "1ece7acaca58fbfcdeaf3a86c9ec1286",
+    "url": "/mclms_prod/static/media/MarketFront.1ece7aca.jpg"
+  },
+  {
+    "revision": "75077bdc919704d83793e7d2e6e489f8",
+    "url": "/mclms_prod/static/media/Profile.75077bdc.png"
+  },
+  {
+    "revision": "aa6ba3bc291b81fc712048c2d2626c74",
+    "url": "/mclms_prod/static/media/gcash.aa6ba3bc.png"
+  },
+  {
+    "revision": "652a652cab9c07fe971448e4f6d96beb",
+    "url": "/mclms_prod/static/media/ops.652a652c.png"
+  },
+  {
+    "revision": "cdb38fd805a2a38b5fc21d18e06c822a",
+    "url": "/mclms_prod/static/media/index.cdb38fd8.png"
+  },
+  {
+    "revision": "473e400f4b61cfd0ee0649f92377b4d1",
+    "url": "/mclms_prod/index.html"
+  }
+];
